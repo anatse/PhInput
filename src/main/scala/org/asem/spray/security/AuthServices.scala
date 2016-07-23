5d3420cd0d6b1a2d5b4692ca2a4a527c6cb05607
gosha-user <anatolse@yandex.ru>
2016-07-23 16:24:07 +0300

    initial comit

--- src/main/scala/org/asem/spray/security/AuthServices.scala
+++ src/main/scala/org/asem/spray/security/AuthServices.scala
@@ -2,7 +2,7 @@ package org.asem.spray.security
 
 import spray.http.MediaTypes._
 import spray.httpx.SprayJsonSupport._
-import spray.routing.{HttpService, Route}
+import spray.routing._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -10,6 +10,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
   * Created by gosha-user on 23.07.2016.
   */
 trait AuthServices extends HttpService with Authenticator {
+
   val authRoutes: Route = authenticate(basicUserAuthenticator) {
     userId => path("users" / Segment) { login =>
       get {
@@ -18,19 +19,19 @@ trait AuthServices extends HttpService with Authenticator {
             "access denied"
           }
         }
-      } ~ post {
-        entity(as[PhUser]) {
-          user => {
-            respondWithMediaType(`application/json`) {
-              complete {
-                user
-              }
-            }
+      } ~ post {addUser(_)}
+    }
+  }
+
+  def addUser(ctx: RequestContext) = {
+    entity(as[PhUser]) {
+      user => {
+        respondWithMediaType(`application/json`) {
+          complete {
+            user
           }
         }
       }
     }
   }
-
-  def
 }
