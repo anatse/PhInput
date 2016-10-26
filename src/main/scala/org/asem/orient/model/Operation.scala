cc55ca926675f0ac6e39a0878d6047ad9ee032a3
avsementsov.sbt <avsementsov.sbt@sbarbank.ru>
2016-10-26 16:24:12 +0300

    change to akka http

--- src/main/scala/org/asem/orient/model/Operation.scala
+++
@@ -1,8 +0,0 @@
-package org.asem.orient.model
-
-import org.asem.orient.model.entities.BaseEntity
-
-/**
-  * Created by gosha-user on 03.09.2016.
-  */
-final case class Operation (message:String, success:Boolean = false) extends BaseEntity
--- src/webapp/app/app/view/ContentPanel.js
+++
@@ -1,9 +0,0 @@
-
-Ext.define('PH.view.ContentPanel', {
-    extend: 'Ext.panel.Panel',
-    xtype: 'contentPanel',
-    title: '',
-    autoScroll: true,
-    layout: 'fit'
-});
-
